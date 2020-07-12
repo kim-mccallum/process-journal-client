4d1b736cd00b3dc8a517816868e138094cb54c14
Kim McCallum <kchaase@gmail.com>
2020-07-11 19:47:04 -0600

    added error boundaries

--- src/Components/RadarChart/RadarChart.test.js
+++ src/Components/RadarChart/RadarChart.test.js
@@ -5,7 +5,6 @@ import RadarChart from "./RadarChart";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  let ctx = document.getElementById("dashboard-radar-chart").getContext("2d");
   ReactDOM.render(
     <BrowserRouter>
       <RadarChart />
